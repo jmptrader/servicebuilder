7d1fe7fb967d85fa6e2f1701ef33ccf3e8b4994a
Andriy Romanov <judo.ras@gmail.com>
2015-04-26 00:11:29 -0700

    Started work on lexer scanning

--- parse/lex.go
+++ parse/lex.go
@@ -2,7 +2,7 @@ package parse
 
 type Lexeme struct {
 	Token Token
-	Value []byte
+	Value String
 	Pos   Position
 }
 
@@ -13,23 +13,33 @@ type Position struct {
 
 // Lexical parser for servicebuilder
 type Lexer struct {
-	Name   String        // Input filename. For error messages
-	Reader *bufio.Reader //  Content reader
-	bus    chan Lexeme   // Lexemes bus is populated with lexemes as they are consumed
+	Name     String        // Input filename. For error messages
+	Reader   *bufio.Reader //  Content reader
+	bus      chan Lexeme   // Lexemes bus is populated with lexemes as they are consumed
+	Position Position
 }
 
 // Return new lexer
 func NewLexer(name String, reader io.Reader) *Lexer {
 	return &Lexer{
-		Name:   name,
-		Reader: bufio.NewReader(reader),
-		bus:    make(chan Lexeme),
+		Name:     name,
+		Reader:   bufio.NewReader(reader),
+		bus:      make(chan Lexeme),
+		Position: &Position{0, 0},
 	}
 }
 
 // Read next rune
 func (self *Lexer) read() rune {
 	ch, _, err := self.Reader.ReadRune()
+	if self.Position != nil {
+		if ch == '\n' {
+			self.Position.Line++
+			self.Position.Column = 0
+		} else {
+			self.Position.Column++
+		}
+	}
 	if err != nil {
 		return eof
 	}
@@ -37,4 +47,60 @@ func (self *Lexer) read() rune {
 }
 
 // Go to the previous rune
-func (self *Lexer) unread() { _ = self.Reader.UnreadRune() }
+func (self *Lexer) unread() {
+	_ = self.Reader.UnreadRune()
+	if self.Position != nil {
+		if self.Position.Column == 0 {
+			if self.Position.Line > 0 {
+				self.Position.Line--
+			}
+		} else {
+			self.Position.Column--
+		}
+	}
+}
+
+func (self *Lexer) Scan() chan Lexeme {
+	go func() {
+		self.scan()
+	}()
+	return self.bus
+}
+
+var eof = rune(0)
+
+func (self *Lexer) scanWhitespace() Lexeme {
+	var buf bytes.Buffer
+	buf.WriteRune(self.read())
+	position := self.Position
+	for {
+		if ch := self.read(); ch == eof {
+			break
+		} else if !isWhitespace(ch) {
+			self.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return Lexeme{WS, buf.String(), position}
+}
+
+func (self *Lexer) scan() {
+ScanLoop:
+	for {
+		ch := self.read()
+		if isWhitespace(ch) {
+			self.unread()
+			self.bus <- self.scanWhitespace()
+		}
+		switch ch {
+		case eof:
+			self.bus <- Lexeme{EOF, "", self.Position}
+			break ScanLoop
+		}
+		self.bus <- Lexeme{ILLEGAL, string(ch), self.Position}
+	}
+	return nil
+}
