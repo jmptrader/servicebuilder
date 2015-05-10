a35f32560feb1b3129e8e9c8c9adeea1d449b1e2
Andriy Romanov <judo.ras@gmail.com>
2015-05-09 22:30:14 -0700

    Added lexer test for pagination

--- parse/lex_test.go
+++ parse/lex_test.go
@@ -16,6 +16,10 @@ func TestLexerScanning(t *testing.T) {
 			"simple.sb",
 			[]Token{MODEL, WS, IDENT, WS, LEFTBRACE, WS, FIELDS, WS, LEFTBRACE, WS, IDENT, COLON, WS, STRING, WS, RIGHTBRACE, WS, RIGHTBRACE, WS, EOF},
 		},
+		{
+			"pagination.sb",
+			[]Token{MODEL, WS, IDENT, WS, LEFTBRACE, WS, FIELDS, WS, LEFTBRACE, WS, IDENT, COLON, WS, STRING, WS, RIGHTBRACE, WS, PAGINATION, WS, LEFTBRACE, WS, IDENT, COLON, WS, NUMERIC, WS, IDENT, COLON, WS, NUMERIC, WS, RIGHTBRACE, WS, RIGHTBRACE, WS, EOF},
+		},
 	}
 	for _, tc := range cases {
 		content, err := ioutil.ReadFile(filepath.Join("test-fixtures", tc.Input))
@@ -27,6 +31,10 @@ func TestLexerScanning(t *testing.T) {
 		i := 0
 		for {
 			lexeme := <-scanner
+			if len(tc.Output) <= i {
+				t.Errorf("Following token was received, but was not expected: %v", lexeme.Token)
+				break
+			}
 			if lexeme.Token != tc.Output[i] {
 				t.Errorf("Expected to get %v token, but got %v", tc.Output[i], lexeme.Token)
 			}
